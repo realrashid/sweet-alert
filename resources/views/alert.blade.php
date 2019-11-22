281b63455e37b7abfb289a3dc9bcaad483a2e47e
thewebartisan7 <57477934+thewebartisan7@users.noreply.github.com>
2019-11-22 12:17:30 +0100

    Same thing refactored
    Just a little bit more short.

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -1,18 +1,9 @@
-@if(!empty(config('sweetalert.animatecss')) && config('sweetalert.animation.enable'))
-    <link rel="stylesheet" href="{{ config('sweetalert.animatecss') }}">
-@endif
-@if (!empty($cdn))
-    @if (Session::has('alert.config'))
-        <script src="{{ $cdn }}"></script>
-        <script>
-            Swal.fire({!! Session::pull('alert.config') !!});
-        </script>
+@if (Session::has('alert.config'))
+    @if(config('sweetalert.animation.enable') && !empty(config('sweetalert.animatecss')))
+        <link rel="stylesheet" href="{{ config('sweetalert.animatecss') }}">
     @endif
-@else
-    @if (Session::has('alert.config'))
-    <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
+    <script src="{{ $cdn?? asset('vendor/sweetalert/sweetalert.all.js')  }}"></script>
     <script>
         Swal.fire({!! Session::pull('alert.config') !!});
     </script>
-    @endif
 @endif
